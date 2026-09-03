7f023b5cd77792bdfc58cc0df58ad6438059f296
Antonio Sediles <torosediles10@gmail.com>
2026-09-03 18:03:55 +0200

    Create commonComposables package.

---
+++ shared/designsystem/src/commonMain/kotlin/org/shareat/shared/designsystem/composables/Dummy.kt
@@ -0,0 +1,10 @@
+package org.shareat.shared.designsystem.composables
+
+import androidx.compose.runtime.Composable
+
+/**
+ * This package is to add common composables between features. They have to be public.
+ */
+@Composable
+public fun Dummy() {
+}
